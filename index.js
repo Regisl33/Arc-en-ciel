5f77ad81d9225207278a67bf5ec52d52f4a8d52b
Regisl33 <reg33gaming@gmail.com>
2023-11-01 15:48:17 -0400

    start of the logic JS

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+let data = [];
+
+class Song {
+  constructor(id, num, title) {
+    this.id = id;
+    this.num = num;
+    this.title = title;
+  }
+}
+
+const utils = {
+  createSong: function () {
+    let id, num, title, song;
+    id = data.length;
+    num = number.value;
+    title = titleInput.value;
+    song = new Song(id, num, title);
+    data.push(song);
+    localStorage.songs = JSON.stringify(data);
+  },
+  getRandomSong: function () {},
+};
+
 const display = {
   lobby: function () {
     app.innerHTML = `
@@ -12,12 +35,14 @@ const display = {
       </div>
     </div>
     `;
+    addSong.addEventListener("click", display.addSongDisplay);
+    randomSong.addEventListener("click", display.randomSongDisplay);
   },
   addSongDisplay: function () {
     app.innerHTML = `
-      <div class="addSong" id="addSong">
+      <div class="addSong">
         <h2>Add a Song</h2>
-        <form action="">
+        <form id="form" action="">
           <label for="number">Number :</label>
           <input
             type="text"
@@ -26,10 +51,10 @@ const display = {
             maxlength="3"
             required
           />
-          <label for="title">Title :</label>
+          <label for="titleInput">Title :</label>
           <input
             type="text"
-            id="title"
+            id="titleInput"
             placeholder="Entre the title of the song"
             required
           />
@@ -37,17 +62,31 @@ const display = {
         </form>
       </div>
     `;
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      utils.createSong();
+      number.value = "";
+      titleInput.value = "";
+    });
   },
   randomSongDisplay: function () {
     app.innerHTML = `
-    <div class="randomSong" id="randomSong">
+    <div class="randomSong">
       <h2>Get Random Songs</h2>
-      <input type="range" value="5" min="1" max="15" />
+      <input id="range" type="range" value="5" min="1" max="15" />
       <span id="rangeDisplay">5</span>
-      <button>Randomize <i class="fa-solid fa-rotate"></i></button>
+      <button id="random">Randomize <i class="fa-solid fa-rotate"></i></button>
       <ul id="output"></ul>
     </div>
     `;
+    range.addEventListener("input", () => {
+      rangeDisplay.textContent = range.value;
+    });
+
+    random.addEventListener("click", utils.getRandomSong);
   },
 };
 display.lobby();
+(() => {
+  if (localStorage.songs) data = JSON.parse(localStorage.songs);
+})();
